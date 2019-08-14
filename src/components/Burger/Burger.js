307a0c1743de0facc604731b295370acff9cb9a2
AD\yros <pis2me@hotmail.com>
2019-08-14 18:42:04 +0100

    Starting the Burger Component.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Burger = () => {
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
+// It si a wrapper for BrgerIngredient components.
+const Burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-buttom" />   
+        </div>
+    );
 };
 
 export default Burger;
