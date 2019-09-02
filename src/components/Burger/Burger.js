92a07f454bdba596b5ad9e2b0a8bda349e88e68e
AD\yros <pis2me@hotmail.com>
2019-09-02 19:00:24 +0100

    Calculating the Ingredient Sm Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,13 +6,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 // It si a wrapper for BrgerIngredient components.
 const Burger = (props) => {
     // return an array of object's keys - transfroms object into an array.
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
      .map(igKey => {
          return [...Array(props.ingredients[igKey])]
          .map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />; 
             });
-     });
+     })
+     .reduce((previousValue, currentValue) => {
+         return previousValue.concat(currentValue)        
+     }, []);
+
+     // .reduce - flat an array of array in one array. [[,,,], [,,,,]] = [,,,,,,,]
+     console.log(transformedIngredients);
+
+     if (transformedIngredients.length === 0) {
+         transformedIngredients = <p>Please start adding ingredients!</p>
+     }
 
     return (
         <div className={classes.Burger}>
@@ -22,5 +32,30 @@ const Burger = (props) => {
         </div>
     );
 };
+
+
+// logs for debging
+// const Burger = (props) => {
+
+//     console.log("Burger.js: props.ingredients", props);
+//     console.log("Burger.js: Object.keys(props.ingredients)", Object.keys(props.ingredients));
+//     // return an array of object's keys - transfroms object into an array.
+//     const transformedIngredients = Object.keys(props.ingredients)
+//      .map(igKey => {
+//         console.log("Burger.js: [...Array(props.ingredients[igKey])]", [...Array(props.ingredients[igKey])]);
+//          return [...Array(props.ingredients[igKey])]
+//          .map((_, i) => {
+//                 return <BurgerIngredient key={igKey + i} type={igKey} />; 
+//             });
+//      });
+
+//     return (
+//         <div className={classes.Burger}>
+//             <BurgerIngredient type="bread-top" />
+//             {transformedIngredients}            
+//             <BurgerIngredient type="bread-bottom" />   
+//         </div>
+//     );
+// };
 
 export default Burger;
