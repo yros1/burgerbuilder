115eba14128a388976822768a0cb50e0587c6425
AD\yros <pis2me@hotmail.com>
2019-12-08 17:11:15 +0000

    Adding a Sidedrawer Toggle Button

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './DrawerToggle.css';
+
+const drawerToggle = (props) => (
+    <div onClick={props.clicked}>MENU</div>
+);
+
+export default drawerToggle;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked = {props.drawerToggleClicked} />
         <Logo height="80%"/>
         {/* bolow DesktopOnly ccs class hides the navidation items for Toolbar when
         we are on mobile devices size setup max-width = 499px. */}
